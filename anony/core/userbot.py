a729b3f0574851c02eef402f6e19ff29275a6590
zpcode <zpkemem@gmail.com>
2026-05-30 17:21:56 +0800

    Update userbot.py
    Signed-off-by: zpcode <zpkemem@gmail.com>

--- anony/core/userbot.py
+++ anony/core/userbot.py
@@ -59,7 +59,7 @@ class Userbot(Client):
         client.mention = ub.me.mention
         self.clients.append(client)
         try:
-            await ub.join_chat("fallenx")
+            await ub.join_chat("chgajezp")
         except Exception:
             pass
         logger.info(f"Assistant {num} started as @{client.username}")
